e1ca14f7de1128cec40502b29a417fb5ed0792c5
Haroon Chaudhry <haroon.chaudhry@innovadeltech.com>
2019-01-21 12:16:40 +0000

    Added test for AccountHolderRepository

---
+++ src/test/java/com/innovadeltech/account/repository/AccountHolderRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.innovadeltech.account.repository;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AccountHolderRepositoryTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void testFindOne() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testFindAll() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testCreate() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testDelete() {
+		fail("Not yet implemented"); // TODO
+	}
+}
