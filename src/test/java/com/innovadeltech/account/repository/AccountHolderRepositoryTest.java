a3c5a761d914263896484a1cc3e847ee7cf3b53d
Haroon Chaudhry <haroon.chaudhry@innovadeltech.com>
2019-01-21 12:31:45 +0000

    Added test cases for findOne of AccountHolderRepository

--- src/test/java/com/innovadeltech/account/repository/AccountHolderRepositoryTest.java
+++ src/test/java/com/innovadeltech/account/repository/AccountHolderRepositoryTest.java
@@ -1,19 +1,42 @@
 package com.innovadeltech.account.repository;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
-import org.junit.BeforeClass;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import com.innovadeltech.account.model.AccountHolder;
+
+import junit.framework.Assert;
 
 public class AccountHolderRepositoryTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	private AccountHolderRepositry repository = new AccountHolderRepositoryImpl();
+	private AccountHolder mockAccountHolder;
+	
+	@Before
+	public void setup() {
+		Map<Integer, AccountHolder> database = new HashMap<>();
+		database.put(1, new AccountHolder(1, "Mark", "Twain", "12345"));
+		database.put(2, new AccountHolder(2, "David", "Wills", "45678"));
+		
+		mockAccountHolder = new AccountHolder(4, "David", "Boon", "14567");
+		repository.setAccountDatabase(database);
 	}
 
 	@Test
-	public void testFindOne() {
-		fail("Not yet implemented"); // TODO
+	public void testFindOneNotFound() {
+		AccountHolder accountHolder = repository.findOne(3);
+		Assert.assertNull(accountHolder);
+	}
+	
+	@Test
+	public void testFindOneFound() {
+		AccountHolder accountHolder = repository.findOne(1);
+		Assert.assertNotNull(accountHolder);
 	}
 
 	@Test
